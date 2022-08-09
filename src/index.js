1dbfd40f1e6b0b1cc56cd61f405f25297002fee8
diegohp <diego.blake11@gmail.com>
2022-08-09 01:50:19 -0500

    [FIX] CORS error

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const routerAPI = require('./routes');
+const cors = require('cors');
 const {
   errorHandler,
   logErrors,
   boomErrors,
 } = require('./middlewares/errors.middlewares');
-const port = '3000';
+const port = process.env.PORT || '3000';
 const app = express();
 app.use(express.json());
 
+//Solving CORS error
+const whitelist = ['http://127.0.0.1:5500', 'https://myapp.co'];
+const options = {
+  origin: function (origin, callback) {
+    console.log(origin);
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+app.use(cors(options));
+
 routerAPI(app);
+
 app.use(logErrors);
 app.use(boomErrors);
 app.use(errorHandler);
